3bf40d3cfa6cea2c8397e2e691c11c03456ee063
Rahul Surasinghe <rsurasin@gmail.com>
2024-11-02 23:02:27 -0400

    feat: Add eslint and prettier config for better formatting and linting rules

--- eslint.config.js
+++ eslint.config.js
@@ -3,26 +3,77 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import react from 'eslint-plugin-react'
+import prettier from 'eslint-plugin-prettier'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default tseslint.config(
   { ignores: ['dist'] },
+  eslintConfigPrettier,
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        React: true,
+      },
+      parser: tseslint.parser,
+      parserOptions: {
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'react': react,
+      'prettier': prettier,
+      '@typescript-eslint': tseslint.plugin,
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
     },
     rules: {
+      // React Hooks rules
       ...reactHooks.configs.recommended.rules,
+
+      // React Refresh rules
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+
+      // React rules
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+
+      // TypeScript rules
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+
+      // Prettier rules
+      'prettier/prettier': ['error', {
+        singleQuote: true,
+        trailingComma: 'es5',
+        tabWidth: 2,
+        semi: true,
+        printWidth: 80,
+        bracketSpacing: true,
+        arrowParens: 'always'
+      }],
+
+      // General rules
+      'no-console': 'warn',
     },
   },
 )
