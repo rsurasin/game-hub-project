bce2da2df01485ed02ba6034c5f361d89e4040c3
Rahul Surasinghe <rsurasin@gmail.com>
2024-11-06 16:34:26 -0500

    feat: Add components for Alert, Button, and ListGroup. Render in root component App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,44 @@
-import Message from './Message';
+import Alert from './components/Alert';
+import Button from './components/Button';
+import ListGroup from './components/ListGroup';
+
+import { useState } from 'react';
 
 function App() {
+  const [activeAlert, setActiveAlert] = useState(false);
+  const items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris'];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  const handleClick = () => {
+    setActiveAlert(false);
+  };
+
   return (
     <div>
-      <Message />
+      {activeAlert && (
+        <Alert onClose={handleClick}>
+          <span>Hello World</span>
+        </Alert>
+      )}
+      <div>
+        <Button
+          color="secondary"
+          onClick={() => {
+            console.log('Clicked');
+            setActiveAlert(true);
+          }}
+        >
+          My Button
+        </Button>
+      </div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
